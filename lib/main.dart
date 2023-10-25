6ff2ebb95e9cfdeee4c6e1aada2db445219dc979
DELL <anjugupta7060@gmail.com>
2023-10-25 19:34:46 +0530

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:we_chat_app/screens/auth/login_scteen.dart';
 import 'package:we_chat_app/utils/colors.dart';
 
 late Size mq;
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
