bf07dba0a8d5874ad4a2e7c2c5a2f4647124c6c4
DELL <anjugupta7060@gmail.com>
2023-10-30 19:27:05 +0530

    chat user model

---
+++ lib/models/chatUser.dart
@@ -0,0 +1,57 @@
+// To parse this JSON data, do
+//
+//     final chatUser = chatUserFromJson(jsonString);
+
+import 'dart:convert';
+
+ChatUser chatUserFromJson(String str) => ChatUser.fromJson(json.decode(str));
+
+String chatUserToJson(ChatUser data) => json.encode(data.toJson());
+
+class ChatUser {
+    String image;
+    String about;
+    String name;
+    String createdAt;
+    String id;
+    String lastActive;
+    bool isOnline;
+    String pushToken;
+    String email;
+
+    ChatUser({
+        required this.image,
+        required this.about,
+        required this.name,
+        required this.createdAt,
+        required this.id,
+        required this.lastActive,
+        required this.isOnline,
+        required this.pushToken,
+        required this.email,
+    });
+
+    factory ChatUser.fromJson(Map<String, dynamic> json) => ChatUser(
+        image: json["image"],
+        about: json["about"],
+        name: json["name"],
+        createdAt: json["created_at"],
+        id: json["id"],
+        lastActive: json["last_active"],
+        isOnline: json["is_online"],
+        pushToken: json["push_token"],
+        email: json["email"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "image": image,
+        "about": about,
+        "name": name,
+        "created_at": createdAt,
+        "id": id,
+        "last_active": lastActive,
+        "is_online": isOnline,
+        "push_token": pushToken,
+        "email": email,
+    };
+}
