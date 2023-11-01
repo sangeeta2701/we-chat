64103ae35d4ba06892648a3d8dce6fe83a7664c3
DELL <anjugupta7060@gmail.com>
2023-11-01 10:31:24 +0530

    error resolve(firestore error)

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -1,11 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:we_chat_app/models/chatUser.dart';
 
-class APIs{
+class APIs {
   //for authentication
   static FirebaseAuth auth = FirebaseAuth.instance;
 
   //for accessing cloud firestore database
-    static FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 
+  static User get user => auth.currentUser!;
+
+  //for checking user exists or not
+  static Future<bool> userExists() async {
+    return (await firestore.collection("users").doc(user.uid).get()).exists;
+  }
+
+  //for creating new users
+  static Future<void> createUser() async {
+    final time = DateTime.now().millisecondsSinceEpoch.toString();
+
+    final chatUser = ChatUser(
+        id: user.uid,
+        name: user.displayName.toString(),
+        email: user.email.toString(),
+        about: "Hey, I'm using We Chat!",
+        image: user.photoURL.toString(),
+        createdAt: time,
+        isOnline: false,
+        lastActive: time,
+        pushToken: '');
+
+    return await firestore
+        .collection('users')
+        .doc(user.uid)
+        .set(chatUser.toJson());
+  }
+  // static Future<void> createUser() async {
+  //   final time = DateTime.now().microsecondsSinceEpoch.toString();
+  //   final chatUser = ChatUser(
+  //       image: user.photoURL ?? "",
+  //       about: "Hey, I am using We Chat",
+  //       name: user.displayName ?? "",
+  //       createdAt: time,
+  //       id: user.uid,
+  //       lastActive: time,
+  //       isOnline: false,
+  //       pushToken: "",
+  //       email: user.email ?? "");
+  //   return await firestore
+  //           .collection("users")
+  //           .doc(user.uid)
+  //           .set(chatUser.toJson());
+  // }
 }
--- lib/screens/auth/login_scteen.dart
+++ lib/screens/auth/login_scteen.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -29,37 +30,93 @@ class _LoginScreenState extends State<LoginScreen> {
     super.initState();
   }
 
+//   _handleGoogleBtnClick() {
+//     //for showing progress indicator
+//     Dialogs.showProgressBar(context);
+//     _signInWithGoogle().then((user) async {
+//       //for hiding progress indicator
+//       Navigator.pop(context);
+//       if (user != null) {
+//         print("User: ${user.user}");
+//         Navigator.pushReplacement(
+//             context, MaterialPageRoute(builder: (_) => HomeScreen()));
+//       } else {
+//         await APIs.createUser().then((value) {
+//           Navigator.pushReplacement(
+//             context, MaterialPageRoute(builder: (_) => HomeScreen()));
+//         });
+//       }
+//     });
+//   }
+
+//   Future<UserCredential?> _signInWithGoogle() async {
+//     try {
+//       await InternetAddress.lookup("google.com");
+//       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+//       final GoogleSignInAuthentication? googleAuth =
+//           await googleUser?.authentication;
+// //create new credential
+//       final credential = GoogleAuthProvider.credential(
+//           accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+// //once signin retun user credential
+//       return await APIs.auth.signInWithCredential(credential);
+//     } catch (e) {
+//       debugPrint("_signInWithGoogle: $e");
+//       Dialogs.showSnackbar(context, "Something went wrong,check internet");
+//       return null;
+//     }
+//   }
+ // handles google login button click
   _handleGoogleBtnClick() {
-    //for showing progress indicator
+    //for showing progress bar
     Dialogs.showProgressBar(context);
-    _signInWithGoogle().then((user) {
-      //for hiding progress indicator
-       Navigator.pop(context);
+
+    _signInWithGoogle().then((user) async {
+      //for hiding progress bar
+      Navigator.pop(context);
+
       if (user != null) {
-        print("User: ${user.user}");
-        Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (_) => HomeScreen()));
+        print('\nUser: ${user.user}');
+        print('\nUserAdditionalInfo: ${user.additionalUserInfo}');
+
+        if ((await APIs.userExists())) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+        } else {
+          await APIs.createUser().then((value) {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+          });
+        }
       }
     });
   }
 
   Future<UserCredential?> _signInWithGoogle() async {
     try {
-      await InternetAddress.lookup("google.com");
+      await InternetAddress.lookup('google.com');
+      // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
           await googleUser?.authentication;
-//create new credential
+
+      // Create a new credential
       final credential = GoogleAuthProvider.credential(
-          accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
-//once signin retun user credential
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
       return await APIs.auth.signInWithCredential(credential);
     } catch (e) {
-      debugPrint("_signInWithGoogle: $e");
-      Dialogs.showSnackbar(context, "Something went wrong,check internet");
+      log('\n_signInWithGoogle: $e');
+      Dialogs.showSnackbar(context, 'Something Went Wrong (Check Internet!)');
       return null;
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
