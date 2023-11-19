9c8314f4aab22478769e5b040e71cb8e5ebd4bbd
DELL <anjugupta7060@gmail.com>
2023-11-19 09:16:17 +0530

    Handle back button & hide keyboard

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,108 +28,134 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Icon(CupertinoIcons.home),
-        title: _isSearching
-            ? Text(
-                "We Chat",
-              )
-            : TextField(
-                autofocus: true,
-                onChanged: (value) {
-                  // search logic
-                  _searchList.clear();
-                  for (var i in list) {
-                    if (i.name.toLowerCase().contains(value.toLowerCase()) ||
-                        i.email.toLowerCase().contains(value.toLowerCase())) {
-                      _searchList.add(i);
-                    }
-                    setState(() {
-                      _searchList;
-                    });
-                  }
-                },
-                style: TextStyle(
-                  fontSize: 16,
-                  letterSpacing: 0.5,
-                ),
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Search Here",
-                ),
-              ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              setState(() {
-                _isSearching = !_isSearching;
-              });
-            },
-            icon: Icon(_isSearching
-                ? CupertinoIcons.clear_circled_solid
-                : Icons.search),
-          ),
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProfileScreen(
-                      user: APIs.me,
+    return GestureDetector(
+      //for hiding keyboard when a tap is detected on screen
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: WillPopScope(
+        //if search is on & back button pressed then close search
+        // or else close current screen on back button click
+        onWillPop: () {
+          if (_isSearching) {
+            setState(() {
+              _isSearching = !_isSearching;
+            });
+            return Future.value(false);
+          } else {
+            return Future.value(true);
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: Icon(CupertinoIcons.home),
+            title: _isSearching
+                ? TextField(
+                    autofocus: true,
+                    onChanged: (value) {
+                      // search logic
+                      _searchList.clear();
+                      for (var i in list) {
+                        if (i.name
+                                .toLowerCase()
+                                .contains(value.toLowerCase()) ||
+                            i.email
+                                .toLowerCase()
+                                .contains(value.toLowerCase())) {
+                          _searchList.add(i);
+                        }
+                        setState(() {
+                          _searchList;
+                        });
+                      }
+                    },
+                    style: TextStyle(
+                      fontSize: 16,
+                      letterSpacing: 0.5,
                     ),
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Here",
+                    ),
+                  )
+                : Text(
+                    "We Chat",
                   ),
-                );
-              },
-              icon: Icon(Icons.more_vert))
-        ],
-      ),
-      body: StreamBuilder(
-          stream: APIs.getAllUsers(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              //if data is loading
-              case ConnectionState.waiting:
-              case ConnectionState.none:
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
+            actions: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    _isSearching = !_isSearching;
+                  });
+                },
+                icon: Icon(_isSearching
+                    ? CupertinoIcons.clear_circled_solid
+                    : Icons.search),
+              ),
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ProfileScreen(
+                          user: APIs.me,
+                        ),
+                      ),
+                    );
+                  },
+                  icon: Icon(Icons.more_vert))
+            ],
+          ),
+          body: StreamBuilder(
+              stream: APIs.getAllUsers(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  //if data is loading
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
 
-              //if data is loaded
-              case ConnectionState.active:
-              case ConnectionState.done:
-                final data = snapshot.data!.docs;
-                list = data.map((e) => ChatUser.fromJson(e.data())).toList();
+                  //if data is loaded
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    final data = snapshot.data!.docs;
+                    list =
+                        data.map((e) => ChatUser.fromJson(e.data())).toList();
 
-                if (list.isNotEmpty) {
-                  return ListView.builder(
-                      padding: EdgeInsets.only(top: mq.height * .02),
-                      physics: BouncingScrollPhysics(),
-                      itemCount:_isSearching? _searchList.length: list.length,
-                      itemBuilder: (context, index) {
-                        return ChatUserCard(
-                          user:_isSearching?_searchList[index] :list[index],
-                        );
-                      });
-                } else {
-                  return Center(
-                      child: Text(
-                    "No connection found",
-                    style: TextStyle(fontSize: 20),
-                  ));
+                    if (list.isNotEmpty) {
+                      return ListView.builder(
+                          padding: EdgeInsets.only(top: mq.height * .02),
+                          physics: BouncingScrollPhysics(),
+                          itemCount:
+                              _isSearching ? _searchList.length : list.length,
+                          itemBuilder: (context, index) {
+                            return ChatUserCard(
+                              user: _isSearching
+                                  ? _searchList[index]
+                                  : list[index],
+                            );
+                          });
+                    } else {
+                      return Center(
+                          child: Text(
+                        "No connection found",
+                        style: TextStyle(fontSize: 20),
+                      ));
+                    }
                 }
-            }
-          }),
-      //floating button to add new user
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: FloatingActionButton(
-          backgroundColor: themeColor,
-          onPressed: () async {
-            await APIs.auth.signOut();
-            await GoogleSignIn().signOut();
-          },
-          child: Icon(Icons.add_comment_rounded),
+              }),
+          //floating button to add new user
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: FloatingActionButton(
+              backgroundColor: themeColor,
+              onPressed: () async {
+                await APIs.auth.signOut();
+                await GoogleSignIn().signOut();
+              },
+              child: Icon(Icons.add_comment_rounded),
+            ),
+          ),
         ),
       ),
     );
